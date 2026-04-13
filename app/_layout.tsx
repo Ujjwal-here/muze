8ecaaeda99407c9178ab43718ac848f23a2e22cc
Ujjwal-here <ujjwalsbx@gmail.com>
2026-04-13 17:01:04 +0530

    setup cabin font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,36 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
+import { useFonts } from "expo-font";
+import {
+  Cabin_400Regular,
+  Cabin_500Medium,
+  Cabin_600SemiBold,
+  Cabin_700Bold,
+} from "@expo-google-fonts/cabin";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    Cabin_400Regular,
+    Cabin_500Medium,
+    Cabin_600SemiBold,
+    Cabin_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <>
       <Stack>
