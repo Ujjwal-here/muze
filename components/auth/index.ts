b60509674edb65ef1e2518f02b79b2d372d7bc40
Ujjwal-here <ujjwalsbx@gmail.com>
2026-04-16 23:55:34 +0530

    chore(index): export DateOfBirthPicker, OtpCodeInput, and related types/constants from form components

---
+++ components/auth/index.ts
@@ -0,0 +1,4 @@
+export { DateOfBirthPicker } from "./DateOfBirthPicker";
+export { OtpCodeInput } from "./OtpCodeInput";
+export type { DateOfBirth } from "./DateOfBirthPicker";
+export { MONTH_INDEX } from "./DateOfBirthPicker";
